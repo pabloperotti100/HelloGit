c10d82688c38d93781ce5ecfa19288195794556e
Martin Marconcini <martin.marconcini@nextive.com>
2011-06-29 18:30:17 +0200

    Added super Pablo
    Signed-off-by: Martin Marconcini <martin.marconcini@nextive.com>

--- src/com/pp/demos/hellogit/Main.java
+++ src/com/pp/demos/hellogit/Main.java
@@ -4,18 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Main extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        initLayout();
-        
-        // HELLO WORLD MY ASS
-        // OK. (another comment)
-        
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initLayout();
+
+		// HELLO WORLD MY ASS
+		// OK. (another comment)
+
+	}
 
 	private void initLayout() {
 		setContentView(R.layout.main);
+		superPerotti();
+	}
+
+	public void superPerotti() {
+
 	}
 }
